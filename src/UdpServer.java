b2d047647f9408e4f0d39d8d2d8fe39da9444bc0
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2016-10-03 12:57:03 -0300

    .

--- src/UdpServer.java
+++ src/UdpServer.java
@@ -3,21 +3,35 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UdpServer {
 
+
+    
     public static void main(String[] args) throws SocketException, IOException {
-       
+    List<Clientes> Lista = new ArrayList();
     DatagramSocket s = new DatagramSocket(4545);
     System.out.println("Servidor esperando conexão.......");
     String envio;
     DatagramPacket recebe = new DatagramPacket(new byte[512], 512);
     while(true){
         s.receive(recebe);
+        Clientes temp = new Clientes();
+        InetAddress IPAddress = recebe.getAddress();
+        temp.setNome(IPAddress.toString());
+        temp.setIP(IPAddress.toString());
+        temp.setPorta(Integer.toString(recebe.getPort()));
+        Lista.add(temp);
         System.out.print("Mensagem recebida: ");
         String received = new String(recebe.getData(), 0, recebe.getLength());
         System.out.println(received); 
+        for (Clientes element : Lista) {
+            System.out.println(element);
+        }
      }
   }
 }
